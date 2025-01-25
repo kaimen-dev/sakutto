495235338820adf94faa95d17f78e8347aed20ad
kaimen-dev <153684659+kaimen-dev@users.noreply.github.com>
2025-01-25 13:15:32 +0900

    設定ファイルの更新、ユーティリティ関数の追加、ボタンコンポーネントの作成、不要なファイルの削除

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { Button } from '@/components/ui/button'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    
+    <Button>HomeView</Button>
   </main>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,20 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+import autoprefixer from "autoprefixer";
+import tailwind from "tailwindcss";
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
     },
   },
-})
+  plugins: [vue(), vueDevTools()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
